daf2d81d9928728dad6362c75ddbcd5f78c2616d
Alex Nielsen <alex.nielsen@gmail.com>
2019-06-23 19:12:55 -0400

    added all files after initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//Body Parser middleware
+app.use(bodyParser.json());
+
+//DB config
+const db = require("./config/keys").mongoURI;
+//Connect to mongo
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("mongo db connected"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server started on port ${port}`));
